f851e7d2b14fcd148528671a0b50f23964514a02
Margaret Wismer <margaretgwismer.gmail.com>
2016-12-31 14:50:15 -0500

    delete post button with modal

--- public/scripts/user.js
+++ public/scripts/user.js
@@ -1,16 +1,8 @@
-// $(document).ready(function(){
-  function setBodyColor(picker) {
-    document.getElementsByTagName('body')[0].style.backgroundColor = '#' + picker.toString()
-  }
-  function setH1Color(picker) {
-    document.getElementById('userH1').style.color = '#' + picker.toString()
-  }
-  function setTextColor(picker) {
-    document.getElementsByTagName('body')[0].style.color = '#' + picker.toString()
-  }
-  //This doesn't work in the .js files. 
-  var color = <%= @colors.to_json %>; 
-  setBodyColor(color.bodyColor);
-  setH1Color(color.h1Color);
-  setTextColor(color.textColor);
-// })
+$(document).ready(function(){
+  $( ".main-delete-btn" ).click(function() {
+     $('#delete-modal').modal("show");
+  });
+  $(".cancel-btn").click(function(){
+     $('#delete-modal').modal("hide");
+  });
+})
